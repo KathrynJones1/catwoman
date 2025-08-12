16a9f75b438cfa43c2e7e4e153fb0793cfcf3ddf
Nestor Espinoza <nespinoza@stsci.edu>
2025-08-12 11:48:58 -0400

    no need to manually update ver

--- docs/conf.py
+++ docs/conf.py
@@ -34,9 +34,9 @@ copyright = '2019, Kathryn Jones & Nestor Espinoza'
 author = 'Kathryn Jones'
 
 # The short X.Y version
-version = '1.0'
+version = '.'.join(catwoman.__version__.split('.')[:2])
 # The full version, including alpha/beta/rc tags
-release = '1.0.14'
+release = catwoman.__version__
 
 
 # -- General configuration ---------------------------------------------------
